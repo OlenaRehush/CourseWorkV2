1c0b7ed0063fd88795777a789e0944fe488c184f
Mykhailo Bartish <mbartish@intellias.com>
2019-04-09 17:37:07 +0300

    fix

--- CourseWork.Api/Controllers/AuthController.cs
+++ CourseWork.Api/Controllers/AuthController.cs
@@ -19,22 +19,20 @@ namespace CourseWork.Api.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]CredentialsRequest credentials)
         {
-            throw new System.Exception();
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
 
-            // if (!this.ModelState.IsValid)
-            // {
-            //     return this.BadRequest(this.ModelState);
-            // }
-               
-            // string token = await this.authService.LoginAsync(credentials.UserName, credentials.Password);
-               
-            // if (token == null)
-            // {
-            //     this.ModelState.TryAddModelError("login_failure", "Invalid username or password.");
-            //     return this.BadRequest(this.ModelState);
-            // }
-               
-            // return this.Ok(token);
+            string token = await this.authService.LoginAsync(credentials.UserName, credentials.Password);
+
+            if (token == null)
+            {
+                this.ModelState.TryAddModelError("login_failure", "Invalid username or password.");
+                return this.BadRequest(this.ModelState);
+            }
+
+            return this.Ok(token);
         }
     }
 }
